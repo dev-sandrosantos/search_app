edb15070b6e1a6823d5e86df718dba78fc8b1458
sandro <dev.sandrosantos@gmail.com>
2021-03-15 11:09:16 -0300

    .

---
+++ lib/Modules/Search/External/DataSouces/GithubDataSouceImpl.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart';
+import 'package:search_app/Modules/Search/Data/DataSouces/Models/ResultSearchModel.dart';
+import 'package:search_app/Modules/Search/Data/DataSouces/SearchDataSource.dart';
+import 'package:search_app/Modules/Search/Domain/Errors/Errors.dart';
+
+extension on String {
+  nameReplaceAll() {
+    return this.replaceAll(" ", "+");
+  }
+}
+
+class GithubDataSouce implements SearchDataSouce {
+  final Dio dio;
+
+  GithubDataSouce(this.dio);
+
+  @override
+  Future<List<ResultSearchModel>> getSearch(String searchText) async {
+    final response = await dio.get(
+        "https://api.github.com/search/users?q=${searchText.nameReplaceAll()}");
+    if (response.statusCode == 200) {
+      final list = (response.data["items"] as List)
+          .map((e) => ResultSearchModel.fromMap(e))
+          .toList();
+      return list;
+    } else {
+      throw DataSouceError();
+    }
+  }
+}
---
+++ test/Modules/Search/External/DataSouces/GithubDataSouce_test.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:search_app/Modules/Search/Domain/Errors/Errors.dart';
+import 'package:search_app/Modules/Search/External/DataSouces/GithubDataSouceImpl.dart';
+
+import '../../Utils/GithubResponse.dart';
+
+class DioMock extends Mock implements Dio {}
+
+void main() {
+  final dio = DioMock();
+  final datasouce = GithubDataSouce(dio);
+  test('should return a list of ResultSearchModel', () {
+    when(dio.get(any)).thenAnswer((_) async =>
+        Response(data: jsonDecode(GithubResponse), statusCode: 200));
+    final result = datasouce.getSearch('Sandro');
+    expect(result, completes);
+  });
+
+  test('should return an error if the status code is different from 200', () {
+    when(dio.get(any))
+        .thenAnswer((_) async => Response(data: null, statusCode: 401));
+    final result = datasouce.getSearch('Sandro');
+    expect(result, throwsA(isA<DataSouceError>()));
+  });
+}
